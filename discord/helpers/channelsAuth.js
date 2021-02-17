8354efa44a68e881d27ec93540a0e53e95391f4a
Paula Lantran <ablette@Paulas-MacBook-Pro.local>
2021-02-17 13:37:50 +0200

    Commands channels with ID and not name

--- discord/helpers/channelsAuth.js
+++ discord/helpers/channelsAuth.js
@@ -18,8 +18,8 @@ exports.authorizedCommandLocations = (client, message) => {
   if (channel.type === 'dm' && client.config.pmBotAuthorized === true)
     return true;
   if (channel.type === 'text') {
-		// We allow commands to be sent through the commands allowed channels.
-		if (client.config.authorizedCommandChannels.includes(channel.name))
+    // We allow commands to be sent through the commands allowed channels.
+		if (client.config.authorizedCommandChannels.includes(channel.id))
 			return true;
 		// We allow commands to be sent through the commands allowed categories.
 		if (channel.parent && client.config.authorizedCommandCategories.includes(channel.parent.name))
@@ -34,8 +34,8 @@ exports.onlyAuthorizeDmOrChannel = (client, message) => {
   if (channel.type === 'dm' && client.config.pmBotAuthorized === true)
     return true;
   if (channel.type === 'text') {
-		// We allow commands to be sent through the commands allowed channels.
-		if (client.config.authorizedCommandChannels.includes(channel.name))
+    // We allow commands to be sent through the commands allowed channels.
+		if (client.config.authorizedCommandChannels.includes(channel.id))
 			return true;
   }
   return false;
