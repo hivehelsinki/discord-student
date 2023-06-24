808bc2e3f849ec158cee99bceb785425e5005c8b
CrispLake <eemilmajuri@gmail.com>
2023-06-25 02:56:54 +0300

    Added automatic threading option Channels that are added in config inside autoThreadChannels field will have the bot starting thread to all non command messages

--- discord/events/messageCreate.js
+++ discord/events/messageCreate.js
@@ -1,11 +1,30 @@
-const { Events } = require('discord.js');
+const { Events, ChannelType } = require('discord.js');
 const { prefix } = require('../config.json');
+
+const autoThreadEnabled = (client, message) => {
+  const channel = message.channel;
+  if (channel.type === ChannelType.GuildText) {
+    if (client.config.autoThreadChannels && client.config.autoThreadChannels.includes(channel.id)) { return true; }
+  }
+  return false;
+};
+
+const startThread = async (message) => {
+  await message.startThread({
+    name: message.content.substring(0, 99)
+  })
+}
 
 module.exports = {
   name: Events.MessageCreate,
   async execute(message, client) {
     if (message.author.bot) return;
-    if (message.content.indexOf(prefix) !== 0) return;
+    if (message.content.indexOf(prefix) !== 0) {
+      if (autoThreadEnabled(client, message) === true) {
+        await startThread(message);
+      }
+      return;
+    }
     if (client.helpers.channelsAuth.authorizedCommandLocations(client, message) === false) return;
 
 
