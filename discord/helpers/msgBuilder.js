045d16a01411d1d00b39a45037b38c4cbc473a1d
Jordane Gengo <jordane.gengo@wolt.com>
2026-06-08 11:34:33 +0300

    fix: fixing embed issue

--- discord/helpers/msgBuilder.js
+++ discord/helpers/msgBuilder.js
@@ -1,12 +1,12 @@
-const Discord = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 const dateFormat = require('dateformat');
 
 exports.eventMessage = (data) => {
-	const eventEmbed = new Discord.MessageEmbed()
+	return new EmbedBuilder()
 		.setColor('#F57C00')
 		.setTitle(data.name)
 		.setURL('https://intra.42.fr/')
-		.setAuthor('Newspaper Hawker', 'http://clipart-library.com/image_gallery/n1648829.jpg')
+		.setAuthor({ name: 'Newspaper Hawker', iconURL: 'http://clipart-library.com/image_gallery/n1648829.jpg' })
 		.setDescription(data.description)
 		.setThumbnail('http://clipart-library.com/image_gallery/n1648829.jpg')
 		.addFields(
@@ -14,21 +14,23 @@ exports.eventMessage = (data) => {
 			{ name: '🌐 Where', value: data.location ? data.location : "Not specified", inline: true },
 			{ name: '👥 People', value: data.max_people ? data.max_people : "Unlimited", inline: true },
 		);
-	return eventEmbed;
 }
 
 exports.plantWateringMessage = (data) => {
-	const embed = new Discord.MessageEmbed()
+	const embed = new EmbedBuilder()
 		.setColor('#4CAF50')
 		.setTitle(`Water ${data.plant_name}`)
 		.setDescription(data.message || 'Time to water this plant!');
 
+	const fields = [];
 	if (data.location) {
-		embed.addField('Location', data.location, true);
+		fields.push({ name: 'Location', value: data.location, inline: true });
 	}
-
 	if (data.due_at) {
-		embed.addField('Due', dateFormat(data.due_at, 'dd/mm/yyyy HH:MM'), true);
+		fields.push({ name: 'Due', value: dateFormat(data.due_at, 'dd/mm/yyyy HH:MM'), inline: true });
+	}
+	if (fields.length) {
+		embed.addFields(fields);
 	}
 
 	return embed;
