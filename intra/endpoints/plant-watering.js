ceb1a44960bf64314c509129fcea31ff3b61573c
Jordane Gengo <jordane.gengo@wolt.com>
2026-06-08 11:37:08 +0300

    use channelType

--- intra/endpoints/plant-watering.js
+++ intra/endpoints/plant-watering.js
@@ -1,3 +1,5 @@
+const { ChannelType } = require('discord.js');
+
 module.exports = async (discordClient, intraConf, req, res) => {
 	const { plant_name, location, message, due_at } = req.body;
 
@@ -7,8 +9,17 @@ module.exports = async (discordClient, intraConf, req, res) => {
 		return;
 	}
 
+	const guild = discordClient.config?.guild;
+	if (!guild) {
+		console.log('plant-watering: bot is not ready');
+		res.sendStatus(503);
+		return;
+	}
+
 	const channelName = intraConf.plantAlertsChannelName || 'plant-watering';
-	const channel = discordClient.channels.cache.find(c => c.name === channelName);
+	const channel = guild.channels.cache.find(
+		c => c.name === channelName && c.type === ChannelType.GuildText
+	);
 
 	if (!channel) {
 		console.log(`plant-watering: channel "${channelName}" not found`);
@@ -19,6 +30,8 @@ module.exports = async (discordClient, intraConf, req, res) => {
 	const msgBuilder = discordClient.helpers.msgBuilder;
 	try {
 		await channel.send({ embeds: [msgBuilder.plantWateringMessage({ plant_name, location, message, due_at })] });
+		const sendChannel = channel.partial ? await channel.fetch() : channel;
+		await sendChannel.send({ embeds: [msgBuilder.plantWateringMessage({ plant_name, location, message, due_at })] });
 		res.sendStatus(200);
 	}
 	catch (error) {
